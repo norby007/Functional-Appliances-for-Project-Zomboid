da00bd0059a546bb9ab8f00f20884006c7c574ff
norby007 <norby007@gmail.com>
2024-01-16 14:04:33 -0800

    Add files via upload

---
+++ FunctionalAppliances/media/lua/client/FAUtilities.lua
@@ -0,0 +1,273 @@
+FA = FA or {}
+
+FA.translation = {
+	sayoptionunavailable = getText("UI_FunctionalAppliances_sayoptionunavailable"),
+	sayneedagenerator = getText("UI_FunctionalAppliances_sayneedagenerator"),
+	sayneedstobeturnedon = getText("UI_FunctionalAppliances_sayneedstobeturnedon"),
+
+	sayneedmissingingredients = getText("UI_FunctionalAppliances_FA.translation.sayneedmissingingredients"),
+
+	dispense = getText("UI_FunctionalAppliances_dispense"),
+	refillsodabottlewith = getText("UI_FunctionalAppliances_refillsodabottlewith"),
+	saysodafountainmustbeinside = getText("UI_FunctionalAppliances_saysodafountainmustbeinside"),
+	saysodafountainneedsmorewater = getText("UI_FunctionalAppliances_saysodafountainneedsmorewater"),
+	saysodafountainneedsplumbing = getText("UI_FunctionalAppliances_saysodafountainneedsplumbing"),
+	saysodafountainneedswaterinjug = getText("UI_FunctionalAppliances_saysodafountainneedswaterinjug"),
+	sayneedsemptywaterorsodabottle = getText("UI_FunctionalAppliances_sayneedsemptywaterorsodabottle"),
+	sayneedfountaincup = getText("UI_FunctionalAppliances_sayneedfountaincup"),
+	noco2tankconnected = getText("UI_FunctionalAppliances_noco2tankconnected"),
+	nosyrupboxesconnected = getText("UI_FunctionalAppliances_nosyrupboxesconnected"),
+	homemadeberryflavoredsoda = getText("UI_FunctionalAppliances_homemadeberryflavoredsoda"),
+	orangesoda = getText("UI_FunctionalAppliances_orangesoda"),
+	lemonlimesoda = getText("UI_FunctionalAppliances_lemonlimesoda"),
+	rootbeer = getText("UI_FunctionalAppliances_rootbeer"),
+	kycola = getText("UI_FunctionalAppliances_kycola"),
+	carbonatedwater = getText("UI_FunctionalAppliances_carbonatedwater"),
+
+	dispensefrom = getText("UI_FunctionalAppliances_dispensefrom"),
+	refillbeerbottlefrom = getText("UI_FunctionalAppliances_refillbeerbottlefrom"),
+	correctbartapfacing = getText("UI_FunctionalAppliances_correctbartapfacing"),
+	pourfromeast = getText("UI_FunctionalAppliances_pourfromeast"),
+	pourfromwest = getText("UI_FunctionalAppliances_pourfromwest"),
+	saybartapnotconnected = getText("UI_FunctionalAppliances_saybartapnotconnected"),
+	saybartapconnectedtoemptykeg = getText("UI_FunctionalAppliances_saybartapconnectedtoemptykeg"),
+	sayneedbeerbottle = getText("UI_FunctionalAppliances_sayneedbeerbottle"),
+	sayneedplasticcuporbeermug = getText("UI_FunctionalAppliances_sayneedplasticcuporbeermug"),
+
+	currentlyoffsettowarm = getText("UI_FunctionalAppliances_currentlyoffsettowarm"),
+	currentlyoffsettohot = getText("UI_FunctionalAppliances_currentlyoffsettohot"),
+	currentlyoffsettocold = getText("UI_FunctionalAppliances_currentlyoffsettocold"),
+	currentlywarmsettooff = getText("UI_FunctionalAppliances_currentlywarmsettooff"),
+	currentlywarmsettohot = getText("UI_FunctionalAppliances_currentlywarmsettohot"),
+	currentlywarmsettocold = getText("UI_FunctionalAppliances_currentlywarmsettocold"),
+	currentlyhotsettooff = getText("UI_FunctionalAppliances_currentlyhotsettooff"),
+	currentlyhotsettowarm = getText("UI_FunctionalAppliances_currentlyhotsettowarm"),
+	currentlyhotsettocold = getText("UI_FunctionalAppliances_currentlyhotsettocold"),
+	currentlycoldsettooff = getText("UI_FunctionalAppliances_currentlycoldsettooff"),
+	currentlycoldsettowarm = getText("UI_FunctionalAppliances_currentlycoldsettowarm"),
+	currentlycoldsettohot = getText("UI_FunctionalAppliances_currentlycoldsettohot"),
+
+	makehotdog = getText("UI_FunctionalAppliances_makehotdog"),
+	cook = getText("UI_FunctionalAppliances_cook"),
+	sausage = getText("UI_FunctionalAppliances_sausage"),
+	viennasausage = getText("UI_FunctionalAppliances_viennasausage"),
+	frankfurter = getText("UI_FunctionalAppliances_frankfurter"),
+	hotdogbun = getText("UI_FunctionalAppliances_hotdogbun"),
+	slicedbread = getText("UI_FunctionalAppliances_slicedbread"),
+
+	vatoillevel = getText("UI_FunctionalAppliances_vatoillevel"),
+	currentlyfullremovevegetableoil = getText("UI_FunctionalAppliances_currentlyfullremovevegetableoil"),
+	currentlyfullremovecookingoil = getText("UI_FunctionalAppliances_currentlyfullremovecookingoil"),
+	currentlyemptyaddvegetableoil = getText("UI_FunctionalAppliances_currentlyemptyaddvegetableoil"),
+	currentlyemptyaddcookingoil = getText("UI_FunctionalAppliances_currentlyemptyaddcookingoil"),
+	currentlyemptyneedsoil = getText("UI_FunctionalAppliances_currentlyemptyneedsoil"),
+	sayvatsemptyneedoil = getText("UI_FunctionalAppliances_sayvatsemptyneedoil"),
+
+	fry = getText("UI_FunctionalAppliances_fry"),
+	potatowedges = getText("UI_FunctionalAppliances_potatowedges"),
+	cutpotatoes = getText("UI_FunctionalAppliances_cutpotatoes"),
+	potatoskins = getText("UI_FunctionalAppliances_potatoskins"),
+	potatopeels = getText("UI_FunctionalAppliances_potatopeels"),
+	batteredonionrings = getText("UI_FunctionalAppliances_batteredonionrings"),
+	batteredbloomingonion = getText("UI_FunctionalAppliances_batteredbloomingonion"),
+	cuttortillas = getText("UI_FunctionalAppliances_cuttortillas"),
+	batteredshrimp = getText("UI_FunctionalAppliances_batteredshrimp"),
+	batteredfishfillet = getText("UI_FunctionalAppliances_batteredfishfillet"),
+	batteredsausage = getText("UI_FunctionalAppliances_batteredsausage"),
+	batteredchicken = getText("UI_FunctionalAppliances_batteredchicken"),
+	batteredchickenfillet = getText("UI_FunctionalAppliances_batteredchickenfillet"),
+	batteredslicedchicken = getText("UI_FunctionalAppliances_batteredslicedchicken"),
+	batteredchickennuggets = getText("UI_FunctionalAppliances_batteredchickennuggets"),
+	doughboys = getText("UI_FunctionalAppliances_doughboys"),
+	doughnuts = getText("UI_FunctionalAppliances_doughnuts"),
+	churros = getText("UI_FunctionalAppliances_churros"),
+	dough = getText("UI_FunctionalAppliances_dough"),
+	smalldough = getText("UI_FunctionalAppliances_smalldough"),
+	breaddough = getText("UI_FunctionalAppliances_breaddough"),
+	pastrydough = getText("UI_FunctionalAppliances_pastrydough"),
+	donutshapeddough = getText("UI_FunctionalAppliances_donutshapeddough"),
+	batteredcheesesticks = getText("UI_FunctionalAppliances_batteredcheesesticks"),
+
+	checkphone = getText("UI_FunctionalAppliances_checkphone"),
+	saythelineisdead = getText("UI_FunctionalAppliances_saythelineisdead"),
+
+	usepopcornmachine = getText("UI_FunctionalAppliances_usepopcornmachine"),
+	sayneedcanopener = getText("UI_FunctionalAppliances_sayneedcanopener"),
+	sayneedpopcornkernels = getText("UI_FunctionalAppliances_sayneedpopcornkernels"),
+
+	check = getText("UI_FunctionalAppliances_check"),
+	fuel = getText("UI_FunctionalAppliances_fuel"),
+	fuelamount = getText("UI_FunctionalAppliances_fuelamount"),
+	condition = getText("UI_FunctionalAppliances_condition"),
+
+	thecurrenttimeis = getText("UI_FunctionalAppliances_thecurrenttimeis"),
+	replacedeadbattery = getText("UI_FunctionalAppliances_replacedeadbattery"),
+	sayneedabattery = getText("UI_FunctionalAppliances_sayneedabattery"),
+
+	grind = getText("UI_FunctionalAppliances_grind"),
+	maketeain = getText("UI_FunctionalAppliances_maketeain"),
+	makeinstantcoffeein = getText("UI_FunctionalAppliances_makeinstantcoffeein"),
+	brewcoffeein = getText("UI_FunctionalAppliances_brewcoffeein"),
+	brewespressoin = getText("UI_FunctionalAppliances_brewespressoin"),
+	makelatte = getText("UI_FunctionalAppliances_makelatte"),
+	makecappuccino = getText("UI_FunctionalAppliances_makecappuccino"),
+	sayneedmorewater = getText("UI_FunctionalAppliances_sayneedmorewater"),
+	mugorcupofespresso = getText("UI_FunctionalAppliances_mugorcupofespresso"),
+	milk = getText("UI_FunctionalAppliances_milk"),
+	coffeebeans = getText("UI_FunctionalAppliances_coffeebeans"),
+	bagofcoffeebeans = getText("UI_FunctionalAppliances_bagofcoffeebeans"),
+	groundcoffeebeans = getText("UI_FunctionalAppliances_groundcoffeebeans"),
+	teabag = getText("UI_FunctionalAppliances_teabag"),
+	instantcoffee = getText("UI_FunctionalAppliances_instantcoffee"),
+	packetofinstantcoffee = getText("UI_FunctionalAppliances_packetofinstantcoffee"),
+	teacup = getText("UI_FunctionalAppliances_teacup"),
+	mug = getText("UI_FunctionalAppliances_mug"),
+	thermos = getText("UI_FunctionalAppliances_thermos"),
+	lowballglass = getText("UI_FunctionalAppliances_lowballglass"),
+	cyanmug = getText("UI_FunctionalAppliances_cyanmug"),
+	redmug = getText("UI_FunctionalAppliances_redmug"),
+	whitemug = getText("UI_FunctionalAppliances_whitemug"),
+	spiffomug = getText("UI_FunctionalAppliances_spiffomug"),
+	needscupormug = getText("UI_FunctionalAppliances_needscupormug")
+}
+
+FA.getJoypadData = function(playerIndex)
+    	if not playerIndex then 
+		playerIndex = 0 
+	end
+
+	return JoypadState.players[playerIndex + 1]
+end
+
+FA.isModEnabled = function(modname)
+    local actmods = getActivatedMods()
+    for i=0, actmods:size()-1, 1 do
+        if actmods:get(i) == modname then
+            return true
+        end
+    end
+    return false
+end
+
+FA.getFrontSquare = function(square, facing)
+	local frontSquare = nil
+	
+	-- Note that when the square is blocked, get direction commands fail
+	if facing == "S" then
+		frontSquare = square:getS()
+	elseif facing == "E" then
+		frontSquare = square:getE()
+	elseif facing == "W" then
+		frontSquare = square:getW()
+	elseif facing == "N" then
+		frontSquare = square:getN()
+	end
+	
+	return frontSquare
+end
+
+FA.getRearSquare = function(square, facing)
+	local rearSquare = nil
+	
+	-- This version ignores blocked squares
+	if facing == "S" then
+		rearSquare = getSquare(square:getX(), square:getY()-1, square:getZ())
+	elseif facing == "E" then
+		rearSquare = getSquare(square:getX()-1, square:getY(), square:getZ())
+	elseif facing == "W" then
+		rearSquare = getSquare(square:getX()+1, square:getY(), square:getZ())
+	elseif facing == "N" then
+		rearSquare = getSquare(square:getX(), square:getY()+1, square:getZ())
+	end
+	
+	return rearSquare
+end
+
+FA.walkToFront = function(player, object)
+	local objectSquare = object:getSquare()
+
+	local spriteName = object:getSprite():getName()
+	if not spriteName then
+		return false
+	end
+
+	local facing = object:getSprite():getProperties():Val("Facing")
+	-- Some objects do not have a facing to them, treat them as any direction availible
+	if not facing then
+		if objectSquare:getE() then
+			facing = "E"
+		elseif objectSquare:getS() then
+			facing = "S"
+		elseif objectSquare:getW() then
+			facing = "W"
+		elseif objectSquare:getN() then
+			facing = "N"
+		end
+	end
+
+	local frontSquare = FA.getFrontSquare(objectSquare, facing)
+	if not frontSquare then
+		return false
+	end
+
+	local turn = FA.getFrontSquare(frontSquare, facing)
+
+	if AdjacentFreeTileFinder.privTrySquare(objectSquare, frontSquare) then
+		ISTimedActionQueue.add(ISWalkToTimedAction:new(player, frontSquare))
+		if turn then
+			player:faceLocation(objectSquare:getX(), objectSquare:getY())
+		end
+		return true
+	end
+	return false
+end
+
+FA.walkToRear = function(player, object)
+	local objectSquare = object:getSquare()
+
+	local spriteName = object:getSprite():getName()
+	if not spriteName then
+		return false
+	end
+
+	local facing = object:getSprite():getProperties():Val("Facing")
+	-- Some objects do not have a facing to them, treat them as any direction availible otherwise swap facing for rear
+	if not facing then
+		if objectSquare:getW() then
+			facing = "W"
+		elseif objectSquare:getN() then
+			facing = "N"
+		elseif objectSquare:getE() then
+			facing = "E"
+		elseif objectSquare:getS() then
+			facing = "S"
+		end
+	else
+		if facing == "E" then
+			facing = "W"
+		elseif facing == "S" then
+			facing = "N"
+		elseif facing == "W" then
+			facing = "E"
+		elseif facing == "N" then
+			facing = "S"
+		end
+
+	end
+
+	local rearSquare = FA.getFrontSquare(objectSquare, facing)
+	if not rearSquare then
+		return false
+	end
+
+	local turn = FA.getFrontSquare(rearSquare, facing)
+
+	if AdjacentFreeTileFinder.privTrySquare(objectSquare, rearSquare) then
+		ISTimedActionQueue.add(ISWalkToTimedAction:new(player, rearSquare))
+		if turn then
+			player:faceLocation(objectSquare:getX(), objectSquare:getY())
+		end
+		return true
+	end
+	return false
+end
---
+++ FunctionalAppliances/media/lua/shared/Definitions/FAContainerButtonIcons.lua
@@ -0,0 +1,20 @@
+require "Definitions/ContainerButtonIcons"
+
+local t = {}
+
+t.popcornMachine = getTexture("media/ui/Container_PopcornMachine.png")
+t.hotdogMachine = getTexture("media/ui/Container_HotdogMachine.png")
+t.papertowelDispenser = getTexture("media/ui/Container_PaperTowelDispenser.png")
+t.napkinDispenser = getTexture("media/ui/Container_NapkinDispenser.png")
+t.slurpNBurp = getTexture("media/ui/Container_SlurpNBurp.png")
+
+ContainerButtonIcons.popcornMachine = t.popcornMachine
+ContainerButtonIcons.hotdogMachine = t.hotdogMachine
+ContainerButtonIcons.papertowelDispenser = t.papertowelDispenser
+ContainerButtonIcons.napkinDispenser = t.napkinDispenser
+ContainerButtonIcons.slurpNBurp = t.slurpNBurp
+
+
+
+
+
